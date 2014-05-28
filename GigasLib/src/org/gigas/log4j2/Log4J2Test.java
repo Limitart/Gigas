efdec582066d9a721e6ef8f3271020b9f8937159
xiaoxiao9876 <Hxhank9876>
2014-05-29 01:08:15 +0800

    log4j simple test

--- GigasLib/src/org/gigas/log4j2/Log4J2Test.java
+++ GigasLib/src/org/gigas/log4j2/Log4J2Test.java
@@ -1,13 +1,24 @@
 package org.gigas.log4j2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Log4J2Test
+ * 
+ * @author hank
+ * 
+ */
 public class Log4J2Test {
+	private static Logger log = LogManager.getLogger(Log4J2Test.class);
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		log.fatal("fatal leve");
+		log.error("error leve");
+		log.warn("warn leve");
+		log.info("info leve");
+		log.debug("debug leve");
+		log.trace("trace leve");
 	}
 
 }
