97c5297a441e050bdffa5d1eda6b5b290b5138a6
xiaoxiao9876 <Hxhank9876>
2014-05-29 04:02:00 +0800

    log4j2.xml

--- GigasLib/src/org/gigas/log4j2/Log4J2Test.java
+++ GigasLib/src/org/gigas/log4j2/Log4J2Test.java
@@ -7,19 +7,26 @@ import org.apache.logging.log4j.Logger;
  * Log4J2Test
  * 
  * @author hank
- *  @since 2014-05-29 01:17:14
+ * @since 2014-05-29 01:17:14
  */
 public class Log4J2Test {
 	private static Logger log = LogManager.getLogger(Log4J2Test.class);
 
 	public static void main(String[] args) {
 		System.out.println("just for log4j2 test");
-		log.fatal("fatal leve");
-		log.error("error leve");
-		log.warn("warn leve");
-		log.info("info leve");
-		log.debug("debug leve");
-		log.trace("trace leve");
+		try {
+			while (true) {
+				log.fatal("fatal leve");
+				log.error("error leve");
+				log.warn("warn leve");
+				log.info("info leve");
+				log.debug("debug leve");
+				log.trace("trace leve");
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			log.error(e, e);
+		}
 	}
 
 }
