a575c07827556e9792dbf6425170acdea4a94d79
xiaoxiao9876 <Hxhank9876>
2014-05-29 02:36:11 +0800

    log4j2

--- GigasLib/src/org/gigas/log4j2/Log4J2Test.java
+++ GigasLib/src/org/gigas/log4j2/Log4J2Test.java
@@ -16,12 +16,12 @@ public class Log4J2Test {
 		System.out.println("just for log4j2 test");
 		try {
 			while (true) {
-				log.fatal("fatal leve");
-				log.error("error leve");
-				log.warn("warn leve");
-				log.info("info leve");
-				log.debug("debug leve");
-				log.trace("trace leve");
+				log.fatal("fatal level");
+				log.error("error level");
+				log.warn("warn level");
+				log.info("info level");
+				log.debug("debug level");
+				log.trace("trace level");
 				Thread.sleep(1000);
 			}
 		} catch (Exception e) {
